d0d7fc1c9815aa783fccbd31acb8df31f86e535e
Matheus <jacominimatheus15@gmail.com>
2025-04-18 18:13:41 -0300

    Learn error handling

--- data/products.js
+++ data/products.js
@@ -131,9 +131,13 @@ export function loadProductsFetch() {
       });
 
       console.log("load products");
-    });
+    }); /*
+    .catch((error) => {
+      console.log("Unexpected error. Please try again later.");
+    }); */
   return promise;
 }
+
 /*
 loadProductsFetch().then(() => {
   console.log("next step");
@@ -156,6 +160,10 @@ export function loadProducts(fun) {
     console.log("load products");
 
     fun();
+  });
+
+  xhr.addEventListener("error", (error) => {
+    console.log("Unexpected error. Please try again later.");
   });
 
   xhr.open("GET", "https://supersimplebackend.dev/products");
